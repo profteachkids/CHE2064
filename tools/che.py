a41036d7afeebc6c7b8a5bb9b720339efc62cfb9
Carlos Co <carloscomail@gmail.com>
2020-11-03 07:13:40 -0500

    VSC.test - initial commit

--- tools/che.py
+++ tools/che.py
@@ -150,7 +150,6 @@ class Props():
                     np.add.at(self.NRTL_alpha, comb_index, alpha)
                     np.add.at(self.NRTL_alpha, (comb_index[1],comb_index[0]), alpha)
 
-        self.PvapVec = jax.vmap(self.Pvap,0)
 
     @partial(jax.jit, static_argnums=(0,))
     def Pvap(self,T):
